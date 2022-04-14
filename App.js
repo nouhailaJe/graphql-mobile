503a4df89806d3bddbfd46d231a2cdd08d91cb2c
JELLOULI Nouhaila <nouhaila.jellouli@fineo.io>
2022-04-14 15:51:59 +0000

    subscription

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
 import { Text, View, Button,TextInput } from 'react-native';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, useMutation } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 // Initialize Apollo Client
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:4000/subscriptions',
+  connectionParams: {
+    authToken: user.authToken,
+  },
+}));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: splitLink,
   cache: new InMemoryCache()
 });
 const GET_ACCOUNTS = gql`
@@ -37,6 +65,34 @@ mutation CreateAccount($name: String) {
   }
 }
 `;
+const ACCOUNTS_SUBSCRIPTION = gql`
+  subscription OnAccountAdded($id: Int!) {
+    accountAdded(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function LatestAccount({ text }) {
+  const { data, loading } = useSubscription(
+    ACCOUNTS_SUBSCRIPTION,
+    { variables: { text } }
+  );
+  return <Text>New Account: {!loading && data.accountAdded.name}</Text>;
+}
+
+const COMMENTS_QUERY = gql`
+  query CommentsForPost($postID: ID!) {
+    post(postID: $postID) {
+      comments {
+        id
+        content
+      }
+    }
+  }
+`;
+
 
 function ExchangeRates() {
   const { loading, error, data,refetch } = useQuery(GET_ACCOUNTS);
@@ -62,7 +118,7 @@ function AddAccount() {
   const { loading, error, data,refetch } = useQuery(GET_ACCOUNTS);
   const [CreateAccount, { }] = useMutation(ADD_ACCOUNT);
   return (
-    <View key={id} style={{ marginTop: 100 ,marginLeft:30}}>
+    <View  style={{ marginTop: 100 ,marginLeft:30}}>
      <TextInput placeholder='account name' value={accountText} onChange={(e)=>setAccountText(e.nativeEvent.text)}></TextInput>
       <Button
         onPress={() => { CreateAccount({

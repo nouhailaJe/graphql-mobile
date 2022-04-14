24b1fd53d1ba286730f7d5d01fd106ebdc71a039
JELLOULI Nouhaila <nouhaila.jellouli@fineo.io>
2022-04-14 14:04:13 +0000

    fineoMobile

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, useMutation } from '@apollo/client';
 
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+const GET_ACCOUNTS = gql`
+  query ExampleQuery {
+    Accounts {
+      id,
+      name,
+      balance
+      transactions {
+        id
+        amount
+        created_at
+      }
+    }
+  }
+`;
+const DELETE_ACCOUNT = gql`
+  mutation DeleteAccount($deleteAccountId: Int!) {
+    deleteAccount(id: $deleteAccountId) {
+      id
+    }
+  }
+`;
+function ExchangeRates() {
+  const { loading, error, data,refetch } = useQuery(GET_ACCOUNTS);
+  const [DeleteAccount, { }] = useMutation(DELETE_ACCOUNT);
+
+  return data !== undefined && data.Accounts.map(({ id, name, balance }) => (
+    <View key={id} style={{ marginTop: 100 }}>
+      <Text>
+        {name}:{balance}
+      </Text>
+      <Button
+        onPress={() => {DeleteAccount({variables: { deleteAccountId: id }});refetch()}}
+        title="Delete"
+        color="#841584"
+      />
+    </View>
+  ));
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <ExchangeRates />
+    </ApolloProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
